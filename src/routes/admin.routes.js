5eb9f1caaf82cb80ec86f428787d91e4fb33db2d
Laza223 <Lazarofeijoo2004@gmail.com>
2024-03-08 14:56:45 -0300

    Uploading Ml-CRUD

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,17 +1,21 @@
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
-const {create, store, edit, update, destroy}= require("../controllers/admin")
 
-/*** CREATE ONE PRODUCT ***/ 
-router.get('/crear-producto/', create); 
-router.post('/crear-producto', store);
+// ************ Controller Require ************
+const { create, store, edit, update, destroy } = require("../controllers/admin");
+const { uploadProducts } = require('../middlewares/uploadFiles');
 
-/*** EDIT ONE PRODUCT ***/ 
-router.get('/editar-producto/:id',edit); 
-router.put('/editar-producto/:id', update); 
+// desde app.js viene con /admin
+/*** CREATE ONE PRODUCT ***/
+router.get('/crear-producto', create);
+router.post('/crear-producto', uploadProducts.single('img'), store);
 
-/*** DELETE ONE PRODUCT***/ 
-router.delete('/eliminar-producto/:id', destroy); 
+/*** EDIT ONE PRODUCT ***/
+router.get('/editar-producto/:id', edit);
+router.put('/editar-producto/:id', uploadProducts.single('img'), update);
+
+/*** DELETE ONE PRODUCT***/
+router.delete('/eliminar-producto/:id', destroy);
 
 module.exports = router;
